cb948e906e5e1e08cd3e2a0ed2ac95923b24ec6c
Jordon Davidson <jodavidson@mta.ca>
2024-11-06 14:55:53 -0400

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Home Manager configuration with Cosmic desktop";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixos-cosmic = {
+      url = "github:lilyinstarlight/nixos-cosmic";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, nixos-cosmic, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+       nixosConfigurations = {
+        # Define your NixOS system configuration
+        nixos = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            {
+              nix.settings = {
+                substituters = [ "https://cosmic.cachix.org/" ];
+                trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+              };
+            }
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            nixos-cosmic.nixosModules.default
+          ];
+        };
+      };
+
+      homeConfigurations = {
+        jordon = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+
+          # Specify your home configuration modules here, for example,
+          # the path to your home.nix.
+          modules = [ ./home.nix ];
+
+          # Optionally use extraSpecialArgs
+          # to pass through arguments to home.nix
+        };
+      };
+    };
+}
