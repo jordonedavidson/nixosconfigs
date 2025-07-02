0d9f6fe228d03ede54c7923011fa19d2ce0c1146
Jordon Davidson <jodavidson@mta.ca>
2025-07-02 11:39:07 -0300

    3rd attempt at meslo nerd font. update pulseaudio declaration

--- configuration.nix
+++ configuration.nix
@@ -62,7 +62,7 @@
 
   # Enable sound with pipewire.
   #sound.enable = true;
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -132,7 +132,7 @@
     fontconfig.enable=true;
     enableDefaultPackages = true;    
     packages = with pkgs; [
-      meslo-nerd-font
+      nerd-fonts.meslo
       fira-code
       fira-code-symbols
     ];
