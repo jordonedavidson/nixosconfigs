37363b36dbad05a86dee3e0d82611e22176e749c
Jordon Davidson <jodavidson@mta.ca>
2025-08-07 09:17:18 -0300

    comment out services.pulseaudio

--- configuration.nix
+++ configuration.nix
@@ -62,7 +62,7 @@
 
   # Enable sound with pipewire.
   #sound.enable = true;
-  services.pulseaudio.enable = false;
+  #services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
